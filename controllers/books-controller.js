6b1feea1e65ca5cbfdd7513242464764819fbe9b
Yusuf Msabeh <yussef.msabeh@gmail.com>
2023-05-31 12:05:16 +0300

    feat: handle and rendering pages using pug

--- controllers/books-controller.js
+++ controllers/books-controller.js
@@ -1,17 +1,33 @@
 const fs = require("fs");
 const path = require("path");
 const crypto = require("crypto");
+const pug = require("pug");
+const redirect = require("../utils/redirect");
 
 exports.getBooks = (request, response) => {
-  response.writeHead(200, "good", { "content-type": "application/json" });
-  response.end(JSON.stringify(getDataFromFile()));
+  const books = getDataFromFile();
+  const template = pug.compileFile("./views/books.pug");
+  const renderedTemplate = template({ books: books });
+  response.writeHead(200, "good", { "content-type": "text/html" });
+  response.end(renderedTemplate);
 };
 
 exports.getBookById = (request, response) => {
   const id = request.url.slice(7);
   const book = getDataFromFile().filter((book) => book.id == id);
-  response.writeHead(200, { "content-type": "application/json" });
-  response.end(JSON.stringify(book));
+  const template = pug.compileFile("./views/book.pug");
+  const renderedTemplate = template({
+    book: book[0],
+  });
+  response.writeHead(200, { "content-type": "text/html" });
+  response.end(renderedTemplate);
+};
+
+exports.getAddBook = (request, response) => {
+  const template = pug.compileFile("./views/add-book.pug");
+  const renderedTemplate = template();
+  response.writeHead(200, { "content-type": "text/html" });
+  response.end(renderedTemplate);
 };
 
 exports.postBooks = (request, response) => {
@@ -21,11 +37,17 @@ exports.postBooks = (request, response) => {
   });
   request.on("end", () => {
     const id = crypto.randomUUID();
-    body = JSON.parse(Buffer.concat(body).toString());
+    body = Buffer.concat(body).toString();
+
+    let name = body
+      .slice(body.indexOf("="))
+      .replaceAll("+", " ")
+      .replace("=", "");
     const books = getDataFromFile();
-    books.push({ id: id, name: body.name });
+    books.push({ id: id, name: name });
     fs.writeFileSync("./data/books.json", JSON.stringify(books));
-    response.end("good");
+
+    redirect(request, response, "add-book");
   });
 };
 

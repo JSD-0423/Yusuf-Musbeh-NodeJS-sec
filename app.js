d34c32fcf09d604bb13593052152816d83fd4ef8
Yusuf Msabeh <yussef.msabeh@gmail.com>
2023-06-01 19:36:26 +0300

    rafactor: assign error route and make main request handler cleaner

--- app.js
+++ app.js
@@ -2,22 +2,25 @@ const http = require("http");
 const booksRoute = require("./routes/books-route");
 const errorRoute = require("./routes/error-route");
 const publicRoute = require("./routes/public-route");
-const pug = require("pug");
 const redirect = require("./utils/redirect");
+const createFileMiddlerWare = require("./middlewares/create-file-middleware");
 
 const PORT = 3000;
 const HOSTNAME = "localhost";
 
 const requestHandler = (request, response) => {
   console.log(request.url);
+  createFileMiddlerWare.createFile(request, response);
   if (request.url == "/") {
     return redirect(request, response, "books");
   }
   if (request.url.startsWith("/public")) {
     return publicRoute(request, response);
   }
-
-  booksRoute(request, response);
+  if (request.url.startsWith("/books") || request.url == "/add-book") {
+    return booksRoute(request, response);
+  }
+  return errorRoute.pageNotFound(request, response);
 };
 const server = http.createServer(requestHandler);
 server.listen(3000, HOSTNAME, PORT, () => {
